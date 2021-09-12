71c939d4a428a70f3bbb5903537ea8440ff4899b
Karen V <karen.pretzel.365@gmail.com>
2021-09-12 09:42:30 -0500

    fixed the route bug

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,12 @@
-//const express = require("express");
-//const express = require('express');
+
 const router = require("express").Router();
 // Import our modular routers for /api/review
 const updateReviewRouter = require('./update-review');
-
-//change the app to the router for best practice
-//const app = express();
-
 // GET Route for update review
-router.use('/review/:id', updateReviewRouter);
-
-//module.exports = app;
-
-
 const movieRouter = require("./moives");
 
 router.use("/movies", movieRouter);
-
+router.use("/review", updateReviewRouter);
 
 
 module.exports = router;
--- routes/update-review.js
+++ routes/update-review.js
@@ -10,16 +10,18 @@ const db = mysql.createConnection(
     password: "harleyHorse",
     database: "movies_db", //database name that you want to connect too
   },
-  console.log(`Connected to the movies_db database.`)
+  console.log(`Connected to the movies_db database.this works`)
 );
 // PUT Route for updating a review name
-updateReview.put('/api/review/:id', (req, res) => {
+updateReview.put('/:id', (req, res) => {
+  console.log("hi")
     const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
     const params = [req.body.review, req.params.id];
   
     db.query(sql, params, (err, result) => {
       if (err) {
         res.status(400).json({ error: err.message });
+        console.log("not working")
       } else if (!result.affectedRows) {
         res.json({
           message: 'Movie not found'
